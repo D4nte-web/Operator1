9380af1df1c8c4181e954a06af4d2196da7b08e7
D4nte-web <murugvo261998@gmail.com>
2025-03-14 16:31:51 +0300

    Ok

--- src/Main.java
+++ src/Main.java
@@ -18,28 +18,49 @@ public class Main {
         }
         System.out.println("Задача 4");
         int year1 = 16;
+        if (year1 > 2 && year1 < 6) {
+            System.out.println("Если возраст человека равен" + year1 + ", то ему нужно ходить в детский сад");
+        }
         if (year1 > 7 && year1 < 17) {
-            System.out.println("Если возраст человека равен 16, то ему нужно ходить в школу.");
+            System.out.println("Если возраст человека равен " + year1 + ", то ему нужно ходить в школу.");
+        }
+        if (year1 >= 18 && year1 <= 24) {
+            System.out.println("Если возраст челвоека равен " + year1 + " , то ему нужно ходить в университет");
+        } else {
+            if (year1 > 24) {
+                System.out.println("Если возраст человека равен" + year1 + ", то ему нужно ходить на работу");
+            }
         }
         System.out.println("Задача 5");
         int kid = 4;
         if (kid < 5) {
-            System.out.println("Если возраст ребёнка равен 4, то ему нельзя кататься на аттракционе.");
+            System.out.println("Если возраст ребёнка равен " + kid + ", то ему нельзя кататься на аттракционе.");
+        }
+        if (kid > 5 && kid < 14) {
+            System.out.println("Если возраст ребёнка равен" + kid + ", то ему можно кататься на аттракционе в сопровождении взрослого");
+        }
+        if (kid > 14) {
+            System.out.println("Если возраст ребёнка равен" + kid + ", то ему можно кататься на аттракционе без сопровождения взрослых");
         }
         System.out.println("Задача 6");
         int places = 54;
         if (places < 60) {
             System.out.println("В вагоне есть сидячее место.");
-        } else {
+        }
+        if (places > 60){
             System.out.println("В вагоен нет сидячих мест.");
+        } else {
+            if (places >= 102){
+                System.out.println("Вагон полностью забит");
+            }
         }
         System.out.println("Задача 7");
         int one = 5;
         int two = 10;
         int three = 15;
-        if (one < two && three > two) {
+        if (one < three && three > two) {
             System.out.println("Число 15 больше остальных");
-        } else if (three < one && one > two) {
+        } else if (three < two && two > one) {
             System.out.println("Число 10 больше остальных");
         } else {
             System.out.println("Число 5 больше остальных");
